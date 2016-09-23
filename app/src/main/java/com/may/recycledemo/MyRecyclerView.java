daa46cd36bf1c71d92b7dd9b863e1b9ce4a07d9c
may666 <598712251@qq.com>
2016-09-23 16:10:29 +0800

    new

--- app/src/main/java/com/may/recycledemo/MyRecyclerView.java
+++ app/src/main/java/com/may/recycledemo/MyRecyclerView.java
@@ -16,6 +16,7 @@ public class MyRecyclerView extends RecyclerView {
 
     public MyRecyclerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
     }
 
     public interface OnItemScrollChangeListener {
