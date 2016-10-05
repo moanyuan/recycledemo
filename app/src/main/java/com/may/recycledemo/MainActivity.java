e23e0c055e2b64d349f2468b5a0f5e4527b59853
may666 <598712251@qq.com>
2016-10-05 16:34:56 +0800

    1111

--- app/src/main/java/com/may/recycledemo/MainActivity.java
+++ app/src/main/java/com/may/recycledemo/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //111
         setContentView(R.layout.activity_main);
         mRecyclerView = (MyRecyclerView) findViewById(R.id.id_recyclerview_horizontal);
         initDatas();
